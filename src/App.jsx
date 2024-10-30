16d6322fe9858f18d907cf3ff2d5eddbff71e88f
Extager <164411975+Extager@users.noreply.github.com>
2024-10-30 15:17:49 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
+import Header from "./components/header/header"
 
 export default function App() {
   return (
     <div className="">
+      <div>
+        <Header />
+      </div>
 
     </div>
   )
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <div className="w-full flex flex-col h-screen justify-center text-center bg-cover bg-center items-center bg-[url('/src/images/Header_Japan.jpg')]">
+            <p className="text-white pt-[10px] pb-5 text-xl ">Меридиан тур</p>
+            <p className="text-white pt-6 pb-6 text-7xl">В самом центре Японии</p>
+            <button className="bg-[#b81a1a] text-white rounded-full px-6 py-3 h-12 w-[125px] text-center " >
+                <span>Готовы?</span>
+            </button>
+        </div>
+    );
+}
