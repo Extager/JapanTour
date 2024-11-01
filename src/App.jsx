8936b3aa8c896b931fe16d86051eb9e813ccd1ed
Extager <164411975+Extager@users.noreply.github.com>
2024-11-01 11:17:49 +0300

    about-block

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Header from "./components/header/header"
+import Header from "./components/header/header";
+import About from './components/main/about/about';
 
 export default function App() {
   return (
     <div className="">
       <div>
         <Header />
+      </div>
+      <div>
+        <About />
       </div>
 
     </div>
