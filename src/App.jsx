2ae3b4890265bd0acd6293ecf702f1dbe941d4ae
Extager <164411975+Extager@users.noreply.github.com>
2024-11-01 11:51:47 +0300

    detailsShort

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/header/header";
 import About from './components/main/about/about';
+import DS from './components/main/details_short/details_short';
 
 export default function App() {
   return (
@@ -9,6 +10,9 @@ export default function App() {
       </div>
       <div>
         <About />
+      </div>
+      <div>
+        <DS />
       </div>
 
     </div>
