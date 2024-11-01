08ab55a11e4bfac335c791063f7c11be7f609547
Extager <164411975+Extager@users.noreply.github.com>
2024-11-01 14:00:05 +0300

    reasons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import Header from "./components/header/header";
 import About from './components/main/about/about';
 import DS from './components/main/details_short/details_short';
+import Reasons from "./components/main/reasons/reasons";
 
 export default function App() {
   return (
     <div className="">
+
       <div>
         <Header />
       </div>
+
       <div>
         <About />
       </div>
+
       <div>
         <DS />
       </div>
 
+      <div>
+        <Reasons />
+      </div>
+      
     </div>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
         <div className="w-full flex flex-col h-screen justify-center text-center bg-cover bg-center items-center 
         bg-[url('/src/images/Header_Japan.jpg')]">
             <p className="text-white pt-[10px] pb-5 text-xl">Меридиан тур</p>
-            <p className="text-white pt-6 pb-6 text-7xl">В самом центре Японии</p>
+            <p className="text-white pt-6 pb-6 text-7xl">«Пять древних столиц Японии»</p>
             <button className="bg-[#b81a1a] text-white rounded-full px-6 py-3 h-12 w-[125px] text-center " >
                 <span>Готовы?</span>
             </button>
