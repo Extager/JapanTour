b6c0f582c4e8790e00d2c758201bd11a8e83add5
Extager <164411975+Extager@users.noreply.github.com>
2024-11-01 21:45:56 +0300

    reason-1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header/header";
 import About from './components/main/about/about';
 import DS from './components/main/details_short/details_short';
 import Reasons from "./components/main/reasons/reasons";
+import Reason_1 from "./components/main/reason_1/reason1";
 
 export default function App() {
   return (
@@ -21,6 +22,10 @@ export default function App() {
 
       <div>
         <Reasons />
+      </div>
+
+      <div>
+        <Reason_1 />
       </div>
       
     </div>
---
+++ src/components/main/reason_1/reason1.jsx
@@ -0,0 +1,15 @@
+export default function Reason_1(){
+    return(
+        <div className="w-[100%] h-[100vh] bg-[url('./images/Reason1.jpeg')] bg-center bg-cover flex justify-center items-center bg-fixed">
+            <div className="bg-white flex items-center justify-center w-[86vh] h-auto px-[60px] py-[60px] text-left leading-12 ">
+                <div className="items-center flex flex-col justify-center">
+                    <p className="text-[42px] py-[20px] font-medium ">Первая причина: новая перспектива</p>
+                    <p className="text-[20px] py-[20px]">Никогда не были в Японии и хотите узнать все за одну поездку?
+                    Или, может, вы были в нескольких городах, но не видели всей картины? 
+                    Если вам интересны культуры других стран, нравится путешествовать на поезде, то этот тур идеален для вас. 
+                    Путешествие «Автостопом по Японии позволит вам прочувствовать каждый город на пути маршрута, а наш гид все подробно расскажет.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
